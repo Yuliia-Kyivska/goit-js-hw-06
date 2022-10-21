105914633f1e29fbdbe008af5dbc1b7fcd3137a0
Yuliia-Kyivska <kiev.lia@gmail.com>
2022-10-21 22:35:52 +0300

    rar

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+let listId = document.querySelector('#categories');
+let liWithId = listId.querySelectorAll('.item');
+console.log('liWithId');
+
+// let items = listWithId.querySelectorAll(".item");
+// for (let item of items) {
+//   let text = item.querySelector('h2').innerHTML;
+//   let count = item.querySelector('li').length;
+
+//   console.log(text);
+// }
--- src/js/task-01.js
+++
@@ -1,9 +0,0 @@
-let listWithId = document.querySelector("#categories");
-let items = listWithId.querySelectorAll(".item");
-for (let item of items) {
-  let text = item.querySelector('h2').innerHTML;
-  let count = item.querySelector('li').length;
-
-  console.log()
-  
-}
