11f7263eaf792bb356323dfc8c542b5b88b70d93
Yuliia-Kyivska <kiev.lia@gmail.com>
2022-10-22 00:24:22 +0300

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+let ulWithId = document.querySelector("#ingredients");
+
+for (let ingredient of ingredients) {
+  let li = document.createElement('li');
+  li.classList.add('item');
+  li.textContent = ingredient;
+  // li.innerHTML = ingredient;
+  // li.append(document.createTextNode(ingredient));
+
+  ulWithId.append(li);
+  
+}
